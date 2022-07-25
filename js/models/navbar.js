3201e8f7dba16f89c42babff1e36d30fbe430e85
sachour <sachour@stanford.edu>
2022-07-25 16:47:17 -0700

    updated navbar to maintain navigation state

--- js/models/navbar.js
+++ js/models/navbar.js
@@ -7,7 +7,7 @@ import "../../lib/backbone.js";
 
 class NavigationEntry {
     constructor({identifier,name,description}){
-        self.id = identifier;
+        this.id = identifier;
         this.name = name;
         this.description = description;
     }
@@ -15,19 +15,65 @@ class NavigationEntry {
 
 }
 
+var dict_len = function(d){
+    return Object.keys(d).length;
+
+}
 $(function(){
-    var NavBarModel = Backbone.Model.extend({
+    var NavModel = Backbone.Model.extend({
         currentState: 0,
+        currentSubstate: -1,
         entries: {},
-        defaults : function (){
-            return [
-                new NavigationEntry(0,"About Me", "who am i?"),
-                new NavigationEntry(1,"Research", "research projects my group is working on"), 
-                new NavigationEntry(2,"Teaching", "classes i'm teaching"), 
-                new NavigationEntry(3,"Service", "professional activities and service")]
+        default_subentries: {},
+        subentries: {},
+        initialize : function (){
+            console.log("instantiate model...");
+            var about_me = this.addOption("About Me");
+            var research = this.addOption("Research");
+            var teaching = this.addOption("Teaching");
+
+            this.addSubOption(research,"Analog Computing", "software methods for analog computing platforms");
+            this.addSubOption(teaching,"CSXXX","advanced software engineering");
+
+            this.set({entries: this.entries, subentries: this.subentries, default_subentries: this.default_subentries})
+            this.select(about_me);
+
         },
-        put : function(entry){
-            entries[entry.id] = entry;
+        navigation_entries: function(){
+            return this.entries.values;
+        },
+        defaults: function(){
+            return {
+                currentState: -1,
+                currentSubstate: -1
+            }
+        },
+        select: function(opt){
+            this.currentState = opt.id;
+            this.currentSubstate = this.default_subentries[opt.id];
+            this.set({currentState: opt.id, 
+                        currentSubstate: this.default_subentries[opt.id]});
+        },
+        subselect: function(subopt){
+            this.currentSubstate = subopt.id;
+            this.set({currentSubstate: this.currentSubstate});
+        },
+        defaultSubOption: function(opt,subopt){
+            this.default_subentries[opt.id] = subopt.id;
+        },
+        addOption: function(opt){
+            var idx = dict_len(this.entries);
+            this.entries[idx] = new NavigationEntry(idx,opt,"");
+            this.default_subentries[idx] = -1;
+            return this.entries[idx];
+        },
+        addSubOption: function(parent,opt,desc){
+            if(!(parent.id in this.subentries)){
+                this.subentries[parent.id] = {};
+            }
+            var idx = dict_len(this.subentries[parent.id]);
+            this.subentries[parent.id][idx] = new NavigationEntry(idx,opt,desc);
+            return this.subentries[idx];
         },
         current : function(){
             return entries[currentState];
@@ -48,6 +94,25 @@ $(function(){
     var NavBarView = Backbone.View.extend({
         el: $("#navbar"),
         subnav: $("#subnavbar"),
+        initialize: function(model){
+            this.model = model;
+            this.render();
+        },
+        render: function(){
+            console.log(this.model);
+            for(var nav in this.model.navigation_entries()){
+                console.log(nav);
+            }
+            console.log("RENDERING NAVBAR");
+            return "FOOBAR"
+        }
+
+
+    })
+
+    var SubNavBarView = Backbone.View.extend({
+        el: $("#navbar"),
+        subnav: $("#subnavbar"),
         initialize: function(){
             console.log("INITIALIZING NAVBAR");
         },
@@ -60,52 +125,19 @@ $(function(){
     })
 
 
+
     var AppView = Backbone.View.extend({
 
         initialize: function(){
             console.log("INITIALIZE APP")
+            this.navmodel = new NavModel();
+            this.navbar = new NavBarView(this.navmodel);
+            this.subnavbar = new SubNavBarView(this.navmodel);
+
         }
 
     })
  
     var App = new AppView;
-    var NavBarView = new NavBarView;
     
 });
-/*
-var TeachingSubnavBar = Backbone.Model.Extend({
-    currentState: null,
-    defaults: function(){
-        return [
-            new NavigationEntry(0,"CSXXX/EEXXX", "Software techniques for emerging hardware platforms"),
-            new NavigationEntry(1,"CSXXX", "Software engineering")
-        ]
-    }
-})
-
-var ResearchSubnavBar = Backbone.Model.Extend({
-    currentState: null,
-    defaults: function(){
-        return [
-            new NavigationEntry(0,"Overview", "overview of current research and publications"),
-            new NavigationEntry(1,"Emerging Hardware", "software for emerging memory and compute technologies"),
-            new NavigationEntry(2,"Analog Computation", "software that enables the use of hardware which performs analog comptuation."),
-            new NavigationEntry(3,"Design Productivity", "software for enabling rapid development of mixed-signal architectures.")
-        ]
-    }
-})
-
-
-var ServiceSubnavBar = Backbone.Model.Extend({
-    currentState: null,
-    defaults : function(){
-        return [
-            new NavigationEntry(0,"Service", "academic service and professional activities"),
-            new NavigationEntry(1,"Talks", "invited talks"),
-            new NavigationEntry(2,"Awards", "awards")
-        ]
-    }
-
-    
-})
-*/
